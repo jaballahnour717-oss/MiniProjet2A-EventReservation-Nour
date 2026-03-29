72d13291fcacb00e8ef7c4f9a0114bd52204e0af
nour <nourjaballah717@gmail.com>
2026-03-29 23:13:50 +0100

    feat(admin): add AdminEventController + all admin templates

---
+++ src/Controller/Admin/AdminSecurityController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class AdminSecurityController extends AbstractController
+{
+    #[Route('/admin/login', name: 'admin_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('admin_dashboard');
+        }
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('admin/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ]);
+    }
+
+    #[Route('/admin/logout', name: 'admin_logout')]
+    public function logout(): void {}
+}
